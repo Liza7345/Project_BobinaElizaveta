a8163faaad0f8ab9384be898afe92a66fa3b05e8
BobinaLiza <bobinaliza14@gmail.com>
2025-09-06 11:16:10 +0300

    Z1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,18 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    println("Введите строку: ")
+    val str = readln()
+    println()
+    var result = ""
+    var count =1
+    for (i in str.indices) {
+        if (i < str.length -1 && str[i] == str[i+1]) {
+            count++
+        } else {
+            result += str[i]
+            if (count > 1) result += count
+            count=1
 
-    println("Program arguments: ${args.joinToString()}")
+        }
+    }
+    println(result)
 }
