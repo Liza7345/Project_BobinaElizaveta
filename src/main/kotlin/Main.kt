f9776a9c7e0f7d3b668a7ec92a0f61a4f0d7ef24
BobinaLiza <bobinaliza14@gmail.com>
2025-09-13 11:04:48 +0300

    Z1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,35 @@
-fun main() {
-    println("Введите строку: ")
-    val str = readln()
-    println()
-    var result = ""
-    var count =1
-    for (i in str.indices) {
-        if (i < str.length -1 && str[i] == str[i+1]) {
-            count++
-        } else {
-            result += str[i]
-            if (count > 1) result += count
-            count=1
+fun compressString (text: String): String {
+    fun compressString (text: String): String {
+        if(text.length <= 1)
+            return text
+        var result = ""
+        var count = 1
+        for (i in 0 until text.length - 1) {
+            if (text[i] == text [i+1]) {
+                count++
+            }
+            else {
+                result += text[i]
+                if (count > 1)
+                    result += count
+                count = 1
 
+            }
         }
+        result += text[text.length -1]
+        if (count > 1) {
+            result += count
+        }
+        return result
     }
-    println(result)
-}
+
+    fun main() {
+        println("Введите строку: ")
+        val str = readln()
+        if (str.isEmpty()) {
+            println ("Вы ввели пустую строку!")
+            return
+        }
+        val result = compressString(str)
+        println("Результат: $result")
+    }
